5c638097da693cc5909461307c71b64f8e4409cd
Marat <goodzilla365@mail.ru>
2019-08-01 10:19:06 +0300

    Подготовка

---
+++ js/backend.js
@@ -0,0 +1,50 @@
+// Выполняемые задачи: загрузка данных
+// Зависимости:
+
+'use strict';
+
+window.backend = (function () {
+
+  var CODE_SUCCESS = 200;
+  var popup = document.querySelector('#messages').content.querySelector('.img-upload__message').cloneNode(true);
+
+  return {
+    load: function (url, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      window.constants.mainTag.appendChild(popup);
+
+      xhr.addEventListener('load', function () {
+        window.constants.mainTag.removeChild(popup);
+        if (xhr.status === CODE_SUCCESS) {
+          onLoad(xhr.response);
+        } else {
+          onError(xhr.status);
+        }
+      });
+
+      xhr.open('GET', url);
+      xhr.send();
+    },
+
+    save: function (url, data, onLoad, onError) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = 'json';
+      window.constants.mainTag.appendChild(popup);
+
+      xhr.addEventListener('load', function () {
+        window.constants.mainTag.removeChild(popup);
+        if (xhr.status === CODE_SUCCESS) {
+          onLoad(xhr.response);
+        } else {
+          onError(xhr.status);
+        }
+      });
+
+      xhr.open('POST', url);
+      xhr.send(data);
+    }
+
+  };
+
+})();
---
+++ js/utils.js
@@ -0,0 +1,164 @@
+// Выполняемые задачи: Утилиты
+// Зависимости: constants.js
+
+'use strict';
+
+window.utils = (function () {
+  var DEBOUNCE_INTERVAL = 500;
+
+  return {
+    // установка задержки
+    setDebounce: function (callback) {
+      var lastTimeout = null;
+
+      return function () {
+        var parameters = arguments;
+        if (lastTimeout) {
+          window.clearTimeout(lastTimeout);
+        }
+        lastTimeout = window.setTimeout(function () {
+          callback.apply(null, parameters);
+        }, DEBOUNCE_INTERVAL);
+      };
+    },
+
+    // показать элемент DOM
+    showElement: function (element) {
+      if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+      }
+    },
+
+    // скрыть элемент DOM
+    hideElement: function (element) {
+      if (!element.classList.contains('hidden')) {
+        element.classList.add('hidden');
+      }
+    },
+
+    // очистка DOM узла
+    removeChildren: function (parent, children) {
+      while (children[0]) {
+        parent.removeChild(children[0]);
+      }
+    },
+
+    // случайное целое число
+    getRandomInteger: function (min, max) {
+      var rand = min + Math.random() * (max + 1 - min);
+      return Math.floor(rand);
+    },
+
+    // перемешивание массива
+    shuffleList: function (list) {
+      var y;
+      var x;
+      for (var i = list.length - 1; i > 0; i--) {
+        y = window.utils.getRandomInteger(0, i);
+        x = list[i];
+        list[i] = list[y];
+        list[y] = x;
+      }
+      return list;
+    },
+
+    // слайдер
+    setSlider: function (pin, track, callback) {
+      pin.addEventListener('mousedown', function (evt) {
+        evt.preventDefault();
+        var startCoords = evt.clientX;
+
+        var onMouseMove = function (moveEvt) {
+          moveEvt.preventDefault();
+
+          var lineWidth = track.clientWidth;
+          var shift = startCoords - moveEvt.clientX;
+
+          startCoords = moveEvt.clientX;
+          callback((pin.offsetLeft - shift) * 100 / lineWidth);
+        };
+
+        var onMouseUp = function (upEvt) {
+          upEvt.preventDefault();
+
+          document.removeEventListener('mousemove', onMouseMove);
+          document.removeEventListener('mouseup', onMouseUp);
+        };
+
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+      });
+    },
+
+    // сообщение об ошибке закрузки
+    onErrorMessage: function (code) {
+      var popup = document.querySelector('#error').content.querySelector('.error').cloneNode(true);
+      var inner = popup.querySelector('.error__inner');
+      var header = inner.querySelector('.error__title');
+
+      var hidePopup = function (element) {
+        window.constants.mainTag.removeChild(element);
+        document.removeEventListener('keydown', onEscPress);
+      };
+
+      var onEscPress = function (evt) {
+        if (evt.keyCode === window.constants.ESC_KEYCODE) {
+          hidePopup(popup);
+        }
+      };
+
+      inner.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        hidePopup(popup);
+      });
+
+      document.addEventListener('keydown', onEscPress);
+
+      popup.addEventListener('click', function (evt) {
+        if (evt.target === popup) {
+          hidePopup(popup);
+        }
+      });
+
+      header.textContent = header.textContent + ': ' + code;
+      window.constants.mainTag.appendChild(popup);
+    },
+
+    // сообщение об успехе закрузки
+    onSuccessMessage: function (code) {
+      var popup = document.querySelector('#success').content.querySelector('.success').cloneNode(true);
+      var inner = popup.querySelector('.success__inner');
+      var button = inner.querySelector('.success__button');
+      var header = inner.querySelector('.success__title');
+
+      var hidePopup = function (element) {
+        window.constants.mainTag.removeChild(element);
+        document.removeEventListener('keydown', onEscPress);
+      };
+
+      var onEscPress = function (evt) {
+        if (evt.keyCode === window.constants.ESC_KEYCODE) {
+          hidePopup(popup);
+        }
+      };
+
+      button.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        hidePopup(popup);
+      });
+
+      document.addEventListener('keydown', onEscPress);
+
+      popup.addEventListener('click', function (evt) {
+        if (evt.target === popup) {
+          hidePopup(popup);
+        }
+      });
+
+      header.textContent = header.textContent + ': ' + code.filename.filename;
+      window.constants.mainTag.appendChild(popup);
+    }
+
+  };
+
+})();
